53ed3f58ed7859534b48c56111bca39ec9f01046
Lucien Shui <lucien@lucien.ink>
2019-08-12 22:32:52 +0800

    stash

---
+++ src/main/java/ink/lucien/aliyun/service/log/AliyunLogServiceMain.java
@@ -0,0 +1,11 @@
+package ink.lucien.aliyun.service.log;
+
+public class AliyunLogServiceMain {
+    public static void main(String[] args) throws Exception {
+        String endpoint = "<log_service_endpoint>";
+        String accessKeyId = "<your_access_key_id>";
+        String accessKeySecret = "<your_access_key_secret>";
+        String project = "<project_name>";
+        String logstore = "<logstore_name>";
+    }
+}
