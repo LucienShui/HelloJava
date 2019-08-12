f6753a03a4901138cd8d2189566e8b005326c559
Lucien Shui <lucien@lucien.ink>
2019-08-12 15:30:24 +0800

    Add @Param in orm.mybatis.annotation

--- src/main/java/ink/lucien/persistent/orm/mybatis/annotation/model/KVSystemMapper.java
+++ src/main/java/ink/lucien/persistent/orm/mybatis/annotation/model/KVSystemMapper.java
@@ -1,20 +1,17 @@
 package ink.lucien.persistent.orm.mybatis.annotation.model;
 
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.annotations.*;
 
 public interface KVSystemMapper {
     @Insert("INSERT INTO `kv_system` (`key`, `value`) VALUES (#{key}, #{value})")
     void insert(KVSystem kvSystem);
 
     @Delete("DELETE FROM `kv_system` WHERE `key` = #{key}")
-    void deleteByKey(String key);
+    void deleteByKey(@Param("key") String key);
 
     @Update("UPDATE `kv_system` SET `value` = #{value} WHERE `key` = #{key}")
     void updateExistRecord(KVSystem key);
 
     @Select("SELECT * FROM `kv_system` WHERE `key` = #{key}")
-    KVSystem getValueByKey(String key);
+    KVSystem getValueByKey(@Param("key") String key);
 }
