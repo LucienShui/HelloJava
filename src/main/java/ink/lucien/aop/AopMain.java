eb332ccdd0b937b2c63e12fce68004989567f522
Lucien Shui <lucien@lucien.ink>
2019-11-02 00:20:09 +0800

    stash

--- src/main/java/ink/lucien/aop/AopMain.java
+++ src/main/java/ink/lucien/aop/AopMain.java
@@ -3,11 +3,13 @@ package ink.lucien.aop;
 import ink.lucien.aop.runner.Runner;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by Lucien on 2019/10/21 12:31
  */
-@ComponentScan
+@Configuration
+@ComponentScan(basePackages = "ink.lucien.aop")
 public class AopMain {
 
     public static void main(String[] args) {
